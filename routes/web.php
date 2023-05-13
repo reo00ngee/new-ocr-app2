d43e181bda8b7bba96fcbe84069a1ed7acd68be0
reo00ngee <reo00ngee@gmail.com>
2023-05-14 05:54:59 +0900

    c

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,7 @@ Route::get('/', function () {
 Auth::routes();
 
 
+
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/ocrResult', [App\Http\Controllers\HomeController::class, 'ocrResult'])->name('ocrResult');
 Route::post('/store', [App\Http\Controllers\HomeController::class, 'store'])->name('store');
